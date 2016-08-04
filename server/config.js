92f46a4e8b30bb1681e56ef83ed6ccd645ffe425
Chuck Pierce <chuck@percolate.com>
2016-08-03 22:39:20 -0400

    edit config for mongo url

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/wedding-website',
+  mongoURL: process.env.MONGO_URI || 'mongodb://localhost:27017/wedding-website',
   port: process.env.PORT || 8000,
 };
 
